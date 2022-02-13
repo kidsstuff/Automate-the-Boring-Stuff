70ebf66fd162ddd9ead287f3e0888f2c19445bec
lh <namesdontmatter@tutanota.com>
2022-02-13 20:00:39 +0100

    table printer done

---
+++ Chapter6/table-printer.py
@@ -0,0 +1,17 @@
+def printTable(tableData):
+    colWidths = [0] * len(tableData)
+    for i in range(len(tableData)):
+        maxLength = 0
+        for data in tableData[i]:
+            if len(data) > maxLength:
+                maxLength = len(data)
+        colWidths[i] = maxLength
+    for j in range(len(tableData[0])):
+        for i in range(len(tableData)):
+            print(tableData[i][j].rjust(colWidths[i]), end=' ')
+        print()
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+printTable(tableData)
