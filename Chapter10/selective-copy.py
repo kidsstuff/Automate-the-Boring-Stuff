f57f002b1ee7d2f135c31c02ba5f45cd36521cc3
lh <namesdontmatter@tutanota.com>
2022-02-17 18:06:58 +0100

    selective copy done

---
+++ Chapter10/selective-copy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# selective-copy.py - walks through a filder tree and searches for files
+# with a certain file extension and copies them in to a new folder
+import shutil, os, re, sys
+from pathlib import Path
+
+def selectiveCopy(source, destination, extension):
+    regex = re.compile(extension)
+    for foldername, subfolders, filenames in os.walk(source):
+        for filename in filenames:
+            if Path(filename).suffix == extension:
+                print(foldername, filename)
+                shutil.copy(Path(foldername) / filename, destination)
+
+if len(sys.argv) == 4:
+    source, destination, extension = Path(sys.argv[1]), Path(sys.argv[2]), sys.argv[3]  
+    if source.exists() and destination.exists():
+        selectiveCopy(source, destination, extension)
+    else:
+        print("Error: invalid path.")
+else:
+    print("Usage: ./selective-copy.py <source> <destination> <extension>")
