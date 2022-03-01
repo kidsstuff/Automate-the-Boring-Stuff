84f8767625f76dacdbb8f1a44d8930cd814b0713
lh <namesdontmatter@tutanota.com>
2022-03-01 18:07:19 +0100

    2048 done

---
+++ Chapter12/2048.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+browser = webdriver.Firefox()
+browser.get('https://gabrielecirulli.github.io/2048/')
+htmlElem = browser.find_element(By.TAG_NAME, 'html')
+while True:
+    htmlElem.send_keys(Keys.UP)
+    htmlElem.send_keys(Keys.RIGHT)
+    htmlElem.send_keys(Keys.DOWN)
+    htmlElem.send_keys(Keys.LEFT)
